377c99cd1fc2e6922b072c32ab02ddc574518568
Equan Pr <equan.p@gmail.com>
2017-06-23 09:35:28 +0700

    Show and update time left per second

--- main.js
+++ main.js
@@ -6,13 +6,17 @@ const url = require('url');
 const Positioner = require("electron-positioner");
 
 const site = require("./dotdpacktpub");
+const { randomColor } = require("./randomcolor");
 
 let browserWindow;
+let dotdBrowserWindow;
+let timeLeft;
+let updateTimeLeft;
+
 let jsonData = { image: "", title: "", description: "", timeLeft: "" };
 
 function createWindow() {
-    browserWindow = new BrowserWindow({ width: 730, height: 270, frame: false, skipTaskbar: true })
-
+    browserWindow = new BrowserWindow({ width: 730, height: 250, frame: false, skipTaskbar: true });
 
     browserWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
@@ -20,23 +24,72 @@ function createWindow() {
         slashes: true
     }))
 
-    browserWindow.webContents.openDevTools();
-
     browserWindow.on('closed', () => {
         browserWindow = null
     })
+
+    browserWindow.webContents.executeJavaScript(`document.body.style.backgroundColor="#${randomColor()}"`);    
+}
+
+function loadDOTD() {
+    dotdBrowserWindow = new BrowserWindow({ show: false });
+    dotdBrowserWindow.loadURL(site.URL);
+    dotdBrowserWindow.webContents.on('dom-ready', (event, url) => {
+        updateTimeLeft = updateTime();
+    })
+}
+
+function updateTime() {
+    let update = setInterval(() => {
+        getTimeLeft();
+    }, 1000);
+    return update;
+}
+
+function getTimeLeft() {
+    if(dotdBrowserWindow != null){
+        dotdBrowserWindow.webContents.executeJavaScript('require("electron").ipcRenderer.send("HTMLData", document.getElementsByClassName("packt-js-countdown")[0].innerHTML);');
+    }
 }
 
 app.on('ready', () => {
+    // Should be scheduled
+    loadDOTD();
+
     createWindow();
+    
+    browserWindow.webContents.once('ready-to-show', () => {
+
+    })
+    
     tray = new Tray("book.png");
     tray.on('click', () => {
-        browserWindow.isVisible() ? browserWindow.hide() : browserWindow.show();
+        if (browserWindow.isVisible()) { 
+            browserWindow.hide();
+            clearInterval(updateTimeLeft);
+        } else { 
+            browserWindow.show();
+            updateTimeLeft = updateTime();
+        };
     })
+
     const contextMenu = Menu.buildFromTemplate([
-        { label: 'Reload', role: 'reload' },
-        { label: 'Exit', role: 'quit' }
+        { label: 'DevTools', click: showDevTools },
+        { label: 'Settings' },
+        { label: 'Autostart', type: 'checkbox' },
+        
+        // No ()
+        { label: 'Exit', click: quitAllWindows }
     ]);
+
+    function quitAllWindows() {
+        dotdBrowserWindow.destroy();
+        browserWindow.destroy();
+    }
+
+    function showDevTools() {
+        browserWindow.webContents.toggleDevTools();
+    }
 
     tray.setContextMenu(contextMenu);
 
@@ -44,7 +97,6 @@ app.on('ready', () => {
     let positioner = new Positioner(browserWindow);
     positioner.move('trayBottomRight', bounds);
 })
-
 
 async function getDOTD() {
     let data = await site.dotdPacktPub();
@@ -57,6 +109,12 @@ ipcMain.on('asyncData', (event, arg) => {
     } catch (error) {
         event.sender.send('asyncMessage', null);
     }
+})
+
+ipcMain.on('HTMLData', (event, arg) => {
+    timeLeft = site.processHTML(arg);
+    let stringCommand = `document.getElementById("book-time-left").innerHTML="${timeLeft}"`;
+    browserWindow.webContents.executeJavaScript(stringCommand);
 })
 
 app.on('window-all-closed', () => {
