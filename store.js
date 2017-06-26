b9f84c8573eab3a294e867c9c7823a663cfe3a0b
Equan Pr <equan.p@gmail.com>
2017-06-26 18:37:25 +0700

    Fix file path

--- store.js
+++ store.js
@@ -9,7 +9,7 @@ class Store {
     constructor(opts) {
         const userDataPath = (electron.app || electron.remote.app).getPath('userData');
 
-        this.path = path.join(userDataPath, `"${opts.configName}.json"`);
+        this.path = path.join(userDataPath, `${opts.configName}.json`);
         this.data = parseDataFile(this.path, opts.defaults);
     }
 
