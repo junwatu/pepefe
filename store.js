c81cb0e173d6f0ff241a30649db79a5ff21d8e22
Equan Pr <equan.p@gmail.com>
2017-06-26 16:12:27 +0700

    Store class

---
+++ store.js
@@ -0,0 +1,34 @@
+// save preferences
+// https://medium.com/@ccnokes/how-to-store-user-data-in-electron-3ba6bf66bc1e
+
+const electron = require("electron");
+const path = require("path");
+const fs = require("fs");
+
+class Store {
+    constructor(opts) {
+        const userDataPath = (electron.app || electron.remote.app).getPath('userData');
+
+        this.path = path.join(userDataPath, `"${opts.configName}.json"`);
+        this.data = parseDataFile(this.path, opts.defaults);
+    }
+
+    get(key) {
+        return this.data[key];
+    }
+
+    set(key, val) {
+        this.data[key] = val;
+        fs.writeFileSync(this.path, JSON.stringify(this.data));
+    }
+}
+
+function parseDataFile(filePath, defaults) {
+    try {
+        return JSON.parse(fs.readFileSync(filePath));
+    } catch (error) {
+        return defaults;
+    }
+}
+
+module.exports = Store;
